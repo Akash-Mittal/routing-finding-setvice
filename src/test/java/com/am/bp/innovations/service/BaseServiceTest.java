6d352c69e7628f8a0b487b5f77886644da829b71
Akash-Mittal <mittal.akash.adam@gmail.com>
2020-06-17 18:34:46 +0200

    Changes:1.) Sample Test Cases.

--- src/test/java/com/am/bp/innovations/service/BaseServiceTest.java
+++ src/test/java/com/am/bp/innovations/service/BaseServiceTest.java
@@ -1,11 +1,25 @@
 package com.am.bp.innovations.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
+import org.junit.Test;
 
-public class BaseServiceTest implements BaseService {
+import com.am.bp.innovations.domain.Location;
+
+public class BaseServiceTest implements BaseService.Functions {
 
     @Before
     public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testCalculateDistanceInKilometer() throws Exception {
+
+        Long distance = calculateDistanceInKilometer(
+                Location.builder().latitude(Double.valueOf("50.023226")).longitude(Double.valueOf("14.439855")).build(),
+                Location.builder().latitude(Double.valueOf("50.023226")).longitude(Double.valueOf("14.439855"))
+                        .build());
+        Assertions.assertThat(distance).isEqualTo(0L);
     }
 
 }
